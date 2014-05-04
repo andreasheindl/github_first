f24a77846c07ea092254ffbfc09be4cebe825a57
Andreas Heindl <andreasheindl@users.noreply.github.com>
2014-05-04 17:04:43 +0200

    init.bat added

---
+++ init.bat
@@ -0,0 +1,8 @@
+git init
+git config user.email "andreasheindl@users.noreply.github.com"
+vim README.md
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/andreasheindl/github_first.git
+git push -u origin master
+
